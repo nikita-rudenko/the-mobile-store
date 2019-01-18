a9e06e3a0756b1346b3b726c67cce7944ab86e84
Nikita R <rdnkta@gmail.com>
2019-01-18 21:59:20 +0200

    setup and dummy data

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const storePhones = [
+	{
+		id: 1,
+		title: 'Google Pixel 3',
+		img: 'img/phone-1.png',
+		price: 799,
+		company: 'Google',
+		info: 'A new way to see the world',
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 2,
+		title: 'Samsung Galaxy Note 9',
+		img: 'img/phone-2.png',
+		price: 999,
+		company: 'Samsung',
+		info: 'The new super powerful Note 9',
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 3,
+		title: 'Apple iPhone XS 64GB',
+		img: 'img/phone-3.png',
+		price: 999,
+		company: 'Apple',
+		info: 'Fastest performance with A12 Bionic',
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 4,
+		title: 'Samsung Galaxy S9 Plus',
+		img: 'img/phone-4.png',
+		price: 840,
+		company: 'Samsung',
+		info: 'The revolutionary camera that adapts like the human eye.',
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 5,
+		title: 'Pocophone F1',
+		img: 'img/product-5.png',
+		price: 300,
+		company: 'Xiaomi',
+		info: 'Master of Speed',
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 6,
+		title: 'Xiaomi Mi 8',
+		img: 'img/phone-6.png',
+		price: 400,
+		company: 'Xiaomi',
+		info: 'Ultra-thin four-sided curved body',
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 7,
+		title: 'Huawei Mate 10 Pro',
+		img: 'img/phone-7.png',
+		price: 800,
+		company: 'Huawei',
+		info: "World's first Kirin AI processor",
+		inCart: false,
+		count: 0,
+		total: 0
+	},
+	{
+		id: 8,
+		title: 'Honor 10 Lite',
+		img: 'img/phone-8.png',
+		price: 330,
+		company: 'Huawei',
+		info: '24MP AI selfie in style',
+		inCart: false,
+		count: 0,
+		total: 0
+	}
+];
+
+export const detailPhone = {
+	id: 1,
+	title: 'Google Pixel 3',
+	img: 'img/phone-1.png',
+	price: 799,
+	company: 'Google',
+	info: 'A new way to see the world',
+	inCart: false,
+	count: 0,
+	total: 0
+};
