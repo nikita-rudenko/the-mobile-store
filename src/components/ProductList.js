3a6ec7d3da0f2f747285db7ff0e8e591aef09722
Nikita R <rdnkta@gmail.com>
2019-01-19 08:46:20 +0200

    Title component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import Product from './Product';
+// import Product from './Product';
+import Title from './Title';
+import { storePhones } from '../data';
 
 export default class ProductList extends Component {
+	state = {
+		products: storePhones
+	};
+
 	render() {
 		return (
-			<div>
-				<h3>
-					<Product />
-				</h3>
-			</div>
+			<React.Fragment>
+				<div className="py-5">
+					<div className="container">
+						<Title name="our" title="products" />
+						<div className="row" />
+					</div>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
