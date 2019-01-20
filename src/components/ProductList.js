cc1a2130b2fb8d48bff8f82e05bc7d88233be15f
Nikita R <rdnkta@gmail.com>
2019-01-20 08:36:14 +0200

    context api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import Product from './Product';
 import Title from './Title';
 import { storePhones } from '../data';
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
 	state = {
@@ -13,8 +14,14 @@ export default class ProductList extends Component {
 			<React.Fragment>
 				<div className="py-5">
 					<div className="container">
-						<Title name="our" title="products" />
-						<div className="row" />
+						<Title text="Welcome to our store!" />
+						<div className="row">
+							<ProductConsumer>
+								{hello => {
+									return <h1>{hello}</h1>;
+								}}
+							</ProductConsumer>
+						</div>
 					</div>
 				</div>
 			</React.Fragment>
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-export default function Title({ name, title }) {
+export default function Title({ text }) {
 	return (
 		<div className="row">
 			<div className="col-10 mx-auto my-2 text-center text-title">
-				<h1 className="text-capitalize font-weight-bold">
-					{name} <strong className="text-blue">{title}</strong>
-				</h1>
+				<h1 className="text-capitalize font-weight-bold">{text}</h1>
 			</div>
 		</div>
 	);
