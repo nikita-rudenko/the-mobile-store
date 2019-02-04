e94dab39ee55990bfabb2386d27cec72c1724ab3
Nikita R <rdnkta@gmail.com>
2019-02-04 17:47:50 +0200

    add back button for emptycart

--- src/components/Cart/EmptyCart.js
+++ src/components/Cart/EmptyCart.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { ButtonContainer } from '../styled/Button';
+import { Link } from 'react-router-dom';
 
 export default function EmptyCart() {
 	return (
 		<div className="container mt-5 pt-3">
 			<div className="row">
-				<div className="col-10 mx-auto text-center text-title">
-					<h2>Your cart is empty</h2>
+				<div className="col-10 mx-auto text-center">
+					<h1>Your cart is empty</h1>
+					<Link to="/">
+						<ButtonContainer>Back To Store</ButtonContainer>
+					</Link>
 				</div>
 			</div>
 		</div>
