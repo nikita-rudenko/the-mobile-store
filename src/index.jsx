faba7118085de5250f4ce2f004f6e7dab5cc991e
Nick Rudenko <rdnkta@gmail.com>
2019-07-28 20:44:07 +0300

    Refactor accordingly to the new config

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,20 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
 import { ProductProvider } from './context';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-	<ProductProvider>
-		<Router>
-			<App />
-		</Router>
-	</ProductProvider>,
-	document.getElementById('root')
+  <ProductProvider>
+    <Router>
+      <App />
+    </Router>
+  </ProductProvider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
